bfe460e39cadeec45c30c09e5d2c9d181cc24e6c
Puneeth N Ganesh <puneethnganesh@Puneeths-MacBook-Air.local>
2025-04-15 22:47:24 +0530

    additional change

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/bbs",
+  base: "/",
   css: {
     modules: {
       scopeBehaviour: "local", // Enables CSS Modules
